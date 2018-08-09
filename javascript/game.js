efbd3fe683708f615d08df7166577c378903e3f4
NasibNia <nasibnia@gmail.com>
2018-08-08 20:59:25 -0700

    first draft, no form yet

---
+++ javascript/game.js
@@ -0,0 +1,16 @@
+var qa = [{ "question"	: " what was the turning point in the history of mankind that settled them down instead nomad life?",
+			"answer"	: "agriculture"},
+		   { "question" : "About how many years ago the most recent Ice Age ended?",
+		   	"answer"	: "11000"
+		   },
+		   {"question" 	: "which of the following is the very unique capability of human being that other species do not own?",
+		   	"answer"	: "myth-making",
+		   },
+		   {"question" 	: "what is the main root of any large-scale human cooperation?",
+		   	"answer"	: "common myth",
+		   },
+]
+
+$(document).ready(function(){
+
+});
